5e71eda01aa72e697503cc1c51ed1e6e86a5a882
Link4Electronics <link4electronics@gmail.com>
2026-02-01 09:26:31 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,6 +8,8 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.kde.audiotube.svg
+export DESKTOP=/usr/share/applications/org.kde.audiotube.desktop
 export DEPLOY_OPENGL=1
 export DEPLOY_PIPEWIRE=1
 
