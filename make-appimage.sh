8534657a2ca677ee5e10e177caed57bd3589cbc2
Link4Electronics <link4electronics@gmail.com>
2026-03-13 08:34:04 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -16,7 +16,7 @@ export DEPLOY_PYTHON=1
 export DEPLOY_QT=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/audiotube /usr/bin/node /usr/include/node/*
+quick-sharun /usr/bin/audiotube /usr/bin/deno
 
 # Additional changes can be done in between here
 
