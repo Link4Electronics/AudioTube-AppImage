de41ec916fa16954f0f95f07b79b67b6f01bf7b5
Link4Electronics <link4electronics@gmail.com>
2026-04-08 19:20:29 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,10 +10,9 @@ export ADD_HOOKS="self-updater.bg.hook:get-yt-dlp.src.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/scalable/apps/org.kde.audiotube.svg
 export DESKTOP=/usr/share/applications/org.kde.audiotube.desktop
-export DEPLOY_OPENGL=1
-export DEPLOY_PIPEWIRE=1
 export DEPLOY_QT=1
 export QT_DIR=qt6
+export DEPLOY_PIPEWIRE=1
 
 # Deploy dependencies
 quick-sharun /usr/bin/audiotube
