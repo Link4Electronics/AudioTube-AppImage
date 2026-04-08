927c50f02499c311bf066dace9eb274b92a561fe
Link4Electronics <link4electronics@gmail.com>
2026-04-08 00:09:05 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -17,7 +17,7 @@ export DEPLOY_QT=1
 
 # Deploy dependencies
 if [ "${ARCH}" = x86_64 ]; then
-    quick-sharun /usr/bin/audiotube /usr/bin/bun /usr/bin/yt-dlp
+    quick-sharun /usr/bin/audiotube /usr/bin/deno /usr/bin/yt-dlp
 else
     quick-sharun /usr/bin/audiotube /usr/bin/node /usr/include/node/*  /usr/bin/yt-dlp
 fi
